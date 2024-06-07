ce5de02aafbea1d840228d0e9204c2f9c457aadb
__filename <filename@inft.kr>
2024-06-07 23:58:11 +0800

    feat: added login page with dark theme

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.html"],
+  theme: {},
+  plugins: [
+    require("daisyui"),
+    require("@catppuccin/tailwindcss")({
+      prefix: "ctp",
+      defaultFlavour: "mocha",
+    }),
+  ],
+};
